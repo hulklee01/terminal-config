4e491248bc9b3ad97af52efa112aec87dcd30352
hulklee01 <sungdl93@gmail.com>
2018-11-08 10:18:45 +0900

    change .sh files

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,3 +6,7 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 # vim vundle install
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # With Vim :PluginInstall
+
+/bin/cp ./.tmux.conf ~/
+/bin/cp ./.vimrc ~/
+/bin/cp ./.zshrc ~/
