6e64ccdcb01f203f777221d0a3679718bc835735
hulklee01 <sungdl93@gmail.com>
2018-11-08 10:08:48 +0900

    first

---
+++ centos.sh
@@ -0,0 +1,7 @@
+sudo -s 
+yum -y update
+yum -y install vim tmux zsh powerline git
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# vim vundle install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
---
+++ ubuntu.sh
@@ -0,0 +1,8 @@
+sudo -s 
+apt-get -y update
+apt-get -y install vim tmux zsh powerline git
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# vim vundle install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# With Vim :PluginInstall
